6aa1f16439a2ca21713df6319e7d6cd85ffcc2a5
AgungRaviano <113073635+AgungRaviano@users.noreply.github.com>
2022-11-16 20:09:11 +0700

    firstcommit

---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function() {
+//     return view('home',
+//     ['name'=> 'Agung',
+//     'alamat'=> 'Depok',
+//     'kelas'=> 'XI PPLG',
+//     'mapel' => ['ipa','ips','mtk']
+//     ]);
+// });
+
+Route::get('/', function() {
+    return view('home');
+})->name('home');
+
+
+Route::get('/', function() {
+    return view('calendar');
+})->name('calendar');
+Route::get('/gallery', function() {
+    return view('gallery');
+})->name('gallery');
+Route::get('/gallery/kanban', function() {
+    return view('kanban');
+})->name('kanban');
+
+
+
+    
+
+// Route::get('/', function () {
+//     return view('homss',[
+//         'name' => 'varel baik hati', 
+//         'role' => 'spy',
+//         'mobil' => ['mercy','avanza','gtr','kijang','brio']
+//     ]);
+// });
